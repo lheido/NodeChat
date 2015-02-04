166e9c1db5c9f7c56160faae3b5ef6188a2aea1c
Florian Perrot <contact@florian-perrot.fr>
2015-02-04 16:25:59 +0100

    Ajout des utils Client et Gestion des messages priver

--- Server.js
+++ Server.js
@@ -3,17 +3,55 @@ var app    = require('express')();
 var server = require('http').Server(app);
 var io     = require('socket.io').listen(server);
 var utils  = require('./utils.js');
+var users = [];
 
-var PORT   = utils.PORT;
+var me = new utils.User("Server","black");
+users.push(me);
+
+function traitementDeLaCommande(commandeUser, currentSocket, currentUser){
+    messageSend = false;
+    if(commandeUser['type'] == '@'){
+        for(var i= 0; i < io.sockets.sockets.length; i++ ){
+            console.log(io.sockets.sockets[i].user.pseudo);
+            if(io.sockets.sockets[i].user.pseudo == commandeUser['commande']){
+                io.sockets.sockets[i].emit(utils.MESSAGE_PRIVATE, commandeUser.arg, currentUser);
+                messageSend = true;
+            }
+        };
+        if(!messageSend)
+            currentSocket.emit(utils.MESSAGE_PRIVATE, "User "+commandeUser.commande+" not exist",me);
+    }
+    else{
+        //TRAITEMENT DE LA COMMANDE
+    }
+}
 
 io.on('connection', function (socket) {
-    socket.on(utils.USER_CONNECTED, function(user){
-        console.log("connect : "+user);
-        io.sockets.emit("user connected", user);
-    });
-    socket.on(utils.USER_DISCONNECTED, function(user){
-        console.log("disconnect : "+user);
-        io.sockets.emit("user disconnected", user);
+    var user;
+    var client = new utils.Client(socket);
+
+    client.onUserConnected = function(user) {
+        this.user = user;
+        users.push(this.user);
+        io.sockets.emit(utils.USER_CONNECTED, this.user);
+        console.log("connect : "+this.user.pseudo);
+    }
+
+    client.onMessageSend = function(msg) {
+        commandeUser = utils.isCommande(msg);
+        if( !commandeUser )
+            io.sockets.emit(utils.MESSAGE_RECEIVED, msg, this.user);
+        else{
+            traitementDeLaCommande(commandeUser, socket, user);
+        }
+    }
+
+    client.init();
+
+    socket.on('disconnect',function(){
+        users.splice(this.user,1);
+        console.log('user disconnected '+this.user.pseudo);
+        io.sockets.emit(utils.USER_DISCONNECTED, this.user);
     });
 });
 
@@ -21,5 +59,5 @@ app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
 });
 
-server.listen(PORT);
-console.log('Server listening on port ' + PORT);
+server.listen(utils.PORT);
+console.log('Server listening on port ' + utils.PORT);
